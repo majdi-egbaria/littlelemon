ad5c04d11fa70d875ecccda49d14c1f3c48b28da
Majdi Egbaria <majdi.egbaria@gmail.com>
2023-11-07 23:37:28 +0200

    routes and static files

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATIC_URL = 'restaurant/static/'
+
+STATICFILES_DIRS = [
+    'restaurant/static/restaurant',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

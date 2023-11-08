5c6b607f4a239ac6b45ca16afff1cf6ebf8fc7cc
Majdi Egbaria <majdi.egbaria@gmail.com>
2023-11-08 17:48:53 +0200

    set mysql connection

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_coursera_meta_capstone',
+        'USER': 'majdi',
+        'PASSWORD': 'BSXa?_9g',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -117,8 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# STATIC_URL = 'restaurant/static/'
 
 STATICFILES_DIRS = [
     'restaurant/static/restaurant',
